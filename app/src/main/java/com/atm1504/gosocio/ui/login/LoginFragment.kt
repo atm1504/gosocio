511ca16b2ae171b514e794b211c464306578f5c0
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-20 17:20:59 +0530

    feat: Login backend base

--- app/src/main/java/com/atm1504/gosocio/ui/login/LoginFragment.kt
+++ app/src/main/java/com/atm1504/gosocio/ui/login/LoginFragment.kt
@@ -5,10 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.atm1504.gosocio.R
+import com.atm1504.gosocio.api.LoginResponse
+import com.atm1504.gosocio.api.RetrofitApi
+import com.atm1504.gosocio.api.SignupResponse
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_signup.*
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginFragment : Fragment() {
 
@@ -22,10 +33,46 @@ class LoginFragment : Fragment() {
         loginViewModel =
             ViewModelProviders.of(this).get(LoginViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_login, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        loginViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginButton.setOnClickListener {
+            val email = email_input.text?.trim().toString()
+            val password = password_input.text?.trim().toString()
+            val err =0
+
+            if(email.isNullOrBlank() || password.isNullOrBlank()){
+                Toast.makeText(context,"Enter allt he fields correctly",Toast.LENGTH_LONG).show()
+            }else{
+                loginUser(email,password)
+            }
+
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        val retofitApi = RetrofitApi.create()
+        val email = RequestBody.create(MediaType.parse("text/plain"), email)
+        val password = RequestBody.create(MediaType.parse("text/plain"), password)
+
+        val call = retofitApi.login(email,password)
+        call.enqueue(object : Callback<LoginResponse> {
+            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(
+                call: Call<LoginResponse>,
+                response: Response<LoginResponse>
+            ) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+    }
+
+
 }
