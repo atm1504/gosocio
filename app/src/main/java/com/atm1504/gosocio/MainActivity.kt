b22194cd1c56c14782b6ed4d96ba9e18da82c8aa
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-20 23:07:53 +0530

    refactor

--- app/src/main/java/com/atm1504/gosocio/MainActivity.kt
+++ app/src/main/java/com/atm1504/gosocio/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_signup,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_tools, R.id.nav_share, R.id.nav_reports_submitted
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
