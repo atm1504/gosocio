f3bd2ac1308cce8838b3bb368da49d315191e853
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-20 19:37:07 +0530

    style: Minor ui changes

--- app/src/main/java/com/atm1504/gosocio/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/atm1504/gosocio/ui/profile/ProfileFragment.kt
@@ -1,31 +1,25 @@
-package com.atm1504.gosocio.ui.tools
+package com.atm1504.gosocio.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.atm1504.gosocio.R
 
-class ToolsFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private lateinit var toolsViewModel: ToolsViewModel
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        toolsViewModel =
-            ViewModelProviders.of(this).get(ToolsViewModel::class.java)
+        profileViewModel =
+            ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tools, container, false)
-        val textView: TextView = root.findViewById(R.id.text_tools)
-        toolsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
--- app/src/main/java/com/atm1504/gosocio/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/atm1504/gosocio/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.atm1504.gosocio.ui.tools
+package com.atm1504.gosocio.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ToolsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is tools Fragment"
