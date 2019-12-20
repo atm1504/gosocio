6d342217cb39b8325f37cfe7dc6f7b8f9dcc5b90
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-20 11:26:26 +0530

    refactor bit

--- app/src/main/java/com/atm1504/gosocio/ui/login/LoginFragment.kt
+++ app/src/main/java/com/atm1504/gosocio/ui/login/LoginFragment.kt
@@ -1,4 +1,4 @@
-package com.atm1504.gosocio.ui.gallery
+package com.atm1504.gosocio.ui.login
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.atm1504.gosocio.R
 
-class GalleryFragment : Fragment() {
+class LoginFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProviders.of(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        loginViewModel =
+            ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(this, Observer {
+        loginViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
