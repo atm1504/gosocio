71578feb9f618c6597e49082f24e320e52b7501d
Amartya Mondal <hayyoulistentome@gmail.com>
2019-12-21 00:25:29 +0530

    feat: show submiited reports

--- app/src/main/java/com/atm1504/gosocio/ui/SubmittedReports/SubmittedReportsFragment.kt
+++ app/src/main/java/com/atm1504/gosocio/ui/SubmittedReports/SubmittedReportsFragment.kt
@@ -1,5 +1,6 @@
 package com.atm1504.gosocio.ui.SubmittedReports
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,10 +10,13 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.atm1504.gosocio.R
 import com.atm1504.gosocio.api.ReportedTasksResponse
 import com.atm1504.gosocio.api.RetrofitApi
 import com.atm1504.gosocio.api.SignupResponse
+import kotlinx.android.synthetic.main.fragment_submitted_reports.*
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType
@@ -22,6 +26,9 @@ import retrofit2.Response
 class SubmittedReportsFragment : Fragment() {
 
     private lateinit var submittedReportsViewModel: SubmittedReportsViewModel
+    private lateinit var submittedReportsAdapter: SubmittedReportsAdapter
+    private lateinit var submittedReportsRecycler: RecyclerView
+    private var progressDialog: ProgressDialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,11 +38,17 @@ class SubmittedReportsFragment : Fragment() {
         submittedReportsViewModel =
             ViewModelProviders.of(this).get(SubmittedReportsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_submitted_reports, container, false)
+        submittedReportsRecycler = root.findViewById(R.id.submitted_reports_recycler_view)
+        submittedReportsAdapter = SubmittedReportsAdapter()
+        submittedReportsRecycler.layoutManager = LinearLayoutManager(context)
+        submittedReportsRecycler.adapter = submittedReportsAdapter
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        message.visibility = View.GONE
 
         fetchReports()
 
@@ -45,6 +58,11 @@ class SubmittedReportsFragment : Fragment() {
         val dummy_access_token = "retyuecu7fb76b46745376453"
         val dummy_email = "reuuye@example.com"
 
+        progressDialog = ProgressDialog(context)
+        progressDialog?.setCancelable(false)
+        progressDialog?.setMessage("Getting list of all reports")
+        progressDialog?.show()
+
         val retrofitApi = RetrofitApi.create()
         val access_token = RequestBody.create(MediaType.parse("text/plain"), dummy_access_token)
         val email = RequestBody.create(MediaType.parse("text/plain"), dummy_email)
@@ -53,15 +71,32 @@ class SubmittedReportsFragment : Fragment() {
 
         call.enqueue(object : retrofit2.Callback<ReportedTasksResponse> {
             override fun onFailure(call: retrofit2.Call<ReportedTasksResponse>, t: Throwable) {
-                Log.d("KHANKI","Failed")
+                Log.d("KHANKI", "Failed")
+                message.visibility = View.VISIBLE
+                message.text = "SOmethign went wrong please try again"
+                progressDialog?.dismiss()
             }
 
             override fun onResponse(
                 call: retrofit2.Call<ReportedTasksResponse>,
                 response: Response<ReportedTasksResponse>
             ) {
-                Toast.makeText(context,"fetched",Toast.LENGTH_LONG).show()
+                progressDialog?.dismiss()
+                Toast.makeText(context, "fetched", Toast.LENGTH_LONG).show()
                 Log.d("KHANKI", response.body()?.toString())
+                if (response.body()?.status == 200) {
+                    val reports = response.body()?.reports
+                    if (reports != null) {
+                        message.visibility = View.GONE
+                        submittedReportsAdapter.setSumittedReportsList(reports)
+                    } else {
+                        message.visibility = View.VISIBLE
+                        message.text = "No reports to show"
+                    }
+                } else {
+                    message.visibility = View.VISIBLE
+                    message.text = "SOmethign went wrong please try again"
+                }
             }
 
         })
